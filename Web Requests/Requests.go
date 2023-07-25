5042372736d8b4fb3cde0ce2890e480138d87d0e
Aman Manwani <aman04.manwani@gmail.com>
2023-07-25 23:51:35 +0530

    added all req

--- Web Requests/Requests.go
+++ Web Requests/Requests.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -13,80 +14,148 @@ const myurl = "https:lco.dev:3000/learn?coursename=reactjs&paymentid=fjfgeiso"
 
 func main() {
 	fmt.Println("Web Requests")
-	response,err := http.Get(urll)
-	
-	if err!=nil {
-		panic(err);
+	response, err := http.Get(urll)
+
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Printf("Response is of type :%T\n",response);
+	fmt.Printf("Response is of type :%T\n", response)
 	// it is important to close all the requests
 	defer response.Body.Close()
 
-	databytes,err := ioutil.ReadAll(response.Body);
+	databytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		panic(err)
 	}
 	content := string(databytes)
-	fmt.Println(content);
+	fmt.Println(content)
 
-	// parsing the url to get all the details 
-	result,_ := url.Parse(myurl)
+	// parsing the url to get all the details
+	result, _ := url.Parse(myurl)
 
-	fmt.Println(result);
+	fmt.Println(result)
 	fmt.Println(result.Scheme)
 	fmt.Println(result.Host)
 	fmt.Println(result.Path)
 	fmt.Println(result.Port())
-	fmt.Println(result.RawQuery);
+	fmt.Println(result.RawQuery)
 
 	// these are key value pairs and we can extract values based like maps
-	qparams := result.Query();
-	fmt.Printf("type of queries is %T",qparams)
+	qparams := result.Query()
+	fmt.Printf("type of queries is %T", qparams)
 
-	fmt.Println(qparams["coursename"]);
+	fmt.Println(qparams["coursename"])
 
-	for _,val := range qparams {
-		fmt.Println("Params is ", val);
+	for _, val := range qparams {
+		fmt.Println("Params is ", val)
 	}
 
 	// to make the url from parts, reverse of above request
 
 	partsOfUrl := &url.URL{
-		Scheme: "https",
-		Host: "lco.dev",
-		Path: "/tutcss",
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
 		RawPath: "user=hitesh",
-	} 
+	}
 
 	anotherURL := partsOfUrl.String()
-	fmt.Println(anotherURL);
+	fmt.Println(anotherURL)
 
 	//get request
-	performGetRequest();
+	// performGetRequest()
 
+	// post request
+	// performPostRequest()
+
+	// postform request
+	// performPostFromReq()
+
+	// json important things
+	coursesDone := []courses{
+		{"Reactjs", 199, "tinki", "180 hrs", []string{"frontend", "starting"}},
+		{"MERN", 299, "tinkiy", "182 hrs", []string{"full stack", "starting"}},
+		{"Go", 99, "tinkiey", "18 hrs", []string{"frontend", "starting"}},
+	}
+
+	// this is not possible or difficiult to read this json
+	// finalJson,_ := json.Marshal(coursesDone);
+
+	// to overcome above problem use this
+	//this function indent our json with spaces as third param
+	finalJson, _ := json.MarshalIndent(coursesDone, "", "\t")
+
+	fmt.Printf("%s", finalJson)
 }
 
-func performGetRequest(){
+type courses struct {
+	Name     string `json:"coursename"`
+	Price    int 
+	// this means we call the platform as website in result
+	platform string `json:"website"`
+	// this means this value should not be reflected on the result
+	time     string `json:"-"`
+	// this means all the json with value as nil will be omitted in the result
+	tags     []string `json:"tags,omitempty"`
+}
+
+func performGetRequest() {
 	const geturl = "https://jsonplaceholder.typicode.com/posts/1"
 
-	response,err := http.Get(geturl);
+	response, err := http.Get(geturl)
 
-	if err!=nil {
-		panic(err);
+	if err != nil {
+		panic(err)
 	}
-	defer response.Body.Close();
+	defer response.Body.Close()
 
-	fmt.Println("Status Cocde: ", response.StatusCode);
-	fmt.Println(response.ContentLength);
+	fmt.Println("Status Cocde: ", response.StatusCode)
+	fmt.Println(response.ContentLength)
 
-	content,_ := ioutil.ReadAll(response.Body);
-	fmt.Println(string(content));
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
 
 	// another way to change bytes into strings is by stringbuilder
 	var responseString strings.Builder
-	byteCnt,_ := responseString.Write(content);
+	byteCnt, _ := responseString.Write(content)
 
-	fmt.Println("ByteCnt is :", byteCnt);
+	fmt.Println("ByteCnt is :", byteCnt)
 	fmt.Println(responseString.String())
+}
+
+func performPostRequest() {
+	const myurll = "http://localhost:8000/post"
+
+	// fake json payload
+	reqBody := strings.NewReader(`
+		{
+			"coursename":"reactjs",
+			"price":0,
+		}
+	`)
+	response, err := http.Post(myurll, "application/json", reqBody)
+
+	if err != nil {
+		panic(err)
+	}
+	content, _ := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	fmt.Println(string(content))
+}
+
+func performPostFromReq() {
+	const myurll = "http://localhost:8000/postform"
+
+	// formdata
+	data := url.Values{}
+	data.Add("firstname", "aman")
+	data.Add("lastname", "manwani")
+	data.Add("email", "ahfgf")
+
+	response, _ := http.PostForm(myurll, data)
+
+	content, _ := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	fmt.Println(string(content))
 }
